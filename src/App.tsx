e39d19e746235ca561b6d406cd2832b4c01e12bf
Raha1362abclabs <raha@abclabs.se>
2023-08-04 10:42:51 +0200

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is my quiz app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
